b519d48ef84414adc841e91e1b1433693bb6e07e
LunArt3481 <b9172148022b@gmail.com>
2026-03-31 16:29:18 +0400

    Add files via upload

---
+++ Program2/Program.cs
@@ -0,0 +1,18 @@
+﻿double price = 1000;
+double percent = 0.02;
+double increase = 0;
+int month_counter = 0;
+int counter = 0;
+for (double i = 0; increase <= 30; month_counter++)
+{
+    increase = (price * percent);
+    i += increase;
+    price += increase;
+    Console.WriteLine($"Цена {price + i:F2}| Увеличение {increase:F2}| Месяц {month_counter}");
+    if (price >= 1200 && counter == 0)
+    {
+        int task_month = month_counter;
+        counter++;
+    }
+}
+Console.WriteLine(task_month);
